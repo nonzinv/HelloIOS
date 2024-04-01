03177be8b3536be44bce17df20122b95f39a1a6a
nonzinv <nonzinv@uw.edu>
2024-03-31 18:34:08 -0700

    code and stuff

---
+++ HelloIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Non on 31/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
